ba93dc22b273b7becf853bc8d4c7d0b1333e7764
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2025-05-14 22:47:56 -0400

    add piece documentation

--- scenes/piece.gd
+++ scenes/piece.gd
@@ -16,15 +16,45 @@ var pieceBlocks: Node3D
 
 ## Reference to the board that the piece is a part of.
 var belongBoard: PlayBoard
+
+## Reference to the [Block]s held as children in the piece.
 var blockCollection: Array[Block]
 
+## Position of the piece on its board.
 var boardPos: Vector2i
+
+## The type of piece that this piece represents.
+## The order should follow:
+## [br][br]
+## 0: O
+## [br][br]
+## 1: J
+## [br][br]
+## 2: L
+## [br][br]
+## 3: Z
+## [br][br]
+## 4: S
+## [br][br]
+## 5: T
+## [br][br]
+## 6: I
 var blockId: int
+
+## The number associated with the piece's rotation. A value of zero represents a rotation where
+## the flattest side of the piece is facing upwards on the board, and is placed as horizontally as possible.
+## Incrementing the value represents a clockwise rotation, decrementing represents a counterclockwise
+## rotation. Should be restricted to values between 0 and 3, inclusive.
 var rotationId: int
 
+## A value of [code]false[\code] indicates the piece is separated from the stack, 
+## [code]true[\code] indicates the piece is slated to become a part of the stack.
 var pieceSet: bool = false
+
+## indicates if the piece is controllable.
 var controllingPiece: bool = true
 
+## Prepares a piece that can be placed into the scene tree immediately.
 static func make_piece(board: PlayBoard, pieceId: int, rotId: int = 0, initBoardPos: Vector2i = Vector2i(3,2), irs: bool = false) -> Piece:
 	var piece: Piece = pieceScene.instantiate()
 	piece.belongBoard = board
@@ -61,6 +91,7 @@ static func make_piece(board: PlayBoard, pieceId: int, rotId: int = 0, initBoard
 func _ready() -> void:
 	pieceBlocks = get_node("PieceBlocks")
 
+## Tests if a piece can move downwards in the [Board] it belongs in.
 func can_move_down() -> bool:
 	var success: bool = false
 	var testPiece: Piece = make_piece(belongBoard, blockId, rotationId, Vector2i(boardPos.x, boardPos.y+1))
@@ -71,13 +102,17 @@ func can_move_down() -> bool:
 	testPiece.queue_free()
 	return success
 
-func attempt_move_piece_horizontally(dir: int) -> void:
+## If able to, moves the piece horizontally by [param dir] blocks.
+func attempt_move_piece_horizontally(dir: int) -> bool:
 	var testPiece: Piece = make_piece(belongBoard, blockId, rotationId, Vector2i(boardPos.x+dir, boardPos.y))
-	if not belongBoard.is_piece_overlapping(testPiece):
+	var success: bool = not belongBoard.is_piece_overlapping(testPiece)
+	if success:
 		transfer_test_piece_data(testPiece)
 		
 	testPiece.queue_free()
+	return success
 
+## If able to, moves the piece down the board by one block.
 func attempt_move_piece_down() -> bool:
 	var success: bool = false
 	var testPiece: Piece = make_piece(belongBoard, blockId, rotationId, Vector2i(boardPos.x, boardPos.y+1))
@@ -89,10 +124,12 @@ func attempt_move_piece_down() -> bool:
 	testPiece.queue_free()
 	return success
 
+## Moves the piece as far down the board as possible.
 func attempt_piece_fast_drop() -> void:
 	while attempt_move_piece_down():
 		pass
 
+## Attempts to apply a rotation in the direction given. Accounts for TGM style rotation cases.
 func attempt_rotate_piece(dir: int) -> void:
 	var newRotId: int = rotationId+dir
 	if newRotId < 0:
@@ -116,7 +153,7 @@ func attempt_rotate_piece(dir: int) -> void:
 		if check_apply_rotation(newRotId, -1):
 			return
 	
-
+## Checks the board for if a given piece is able to rotate there according to TGM rules. Only used for L, J and T pieces.
 func check_apply_rotation(rot: int, dir: int) -> bool:
 	var testPiece: Piece = make_piece(belongBoard, blockId, rot, Vector2i(boardPos.x+dir, boardPos.y))
 	if not belongBoard.is_piece_overlapping(testPiece):
@@ -127,7 +164,7 @@ func check_apply_rotation(rot: int, dir: int) -> bool:
 		testPiece.queue_free()
 		return false
 
-
+## Applies functional piece data from a test piece.
 func transfer_test_piece_data(testPiece: Piece) -> void:
 	boardPos = testPiece.boardPos
 	rotationId = testPiece.rotationId
